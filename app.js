332a78ba3db71df34b32da30c31dd5163aa6e61b
HarryAhnHS <hongsuhahn@gmail.com>
2024-08-17 12:45:09 +0100

    price form validation

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/', (req,res) => {
+    res.redirect('/items');
+})
+
 app.use('/items', itemsRouter);
 app.use('/categories', categoriesRouter);
 
